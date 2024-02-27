1f1238ebffab4d7e4a5f10682ad0e37e09d691c8
Leo Haruyuki <161147808+LeoHaruyuki@users.noreply.github.com>
2024-02-27 06:19:18 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,158 +1,167 @@
+# ---- 
+# Hard rewrite of the originals
+# ----
+# I want to make the code look nicer, but that's something I'll do later
+
+from discord.ext import commands, tasks
+from discord.commands import Option
 from dotenv import load_dotenv
-from discord.ext import tasks, commands
 
+import datetime
 import discord
-import time
-import sqlite3
-import os
 import gspread
-
-bot_channel, log_channel, role, join_list, daily_messages, daily_seconds, voice_dict = None, None, None, [], 0, 0, {}
+import os
+import sys
+import sqlite3
 
 load_dotenv()
-client = commands.Bot(command_prefix="y?!|", intents=discord.Intents.all())
+client = commands.Bot(command_prefix="y|", intents=discord.Intents.all())
 
-last_row = 9
+conn = sqlite3.connect('RIT.db')
+c = conn.cursor()
+c.execute("""CREATE TABLE IF NOT EXISTS user_stats(
+          id TEXT PRIMARY KEY,
+          date TEXT,
+          message_count INTEGER,
+          voice_duration INTEGER,
+          voice_start TEXT
+);""")
+# Ease of access, not technically needed
+c.execute("""CREATE TABLE IF NOT EXISTS server_stats(
+          date TEXT PRIMARY KEY,
+          message_count INTEGER,
+          voice_duration INTEGER
+);""")
+c.execute("""CREATE TABLE IF NOT EXISTS members(
+          member_count INTEGER PRIMARY KEY,
+          time TEXT
+);""")
+conn.commit()
+
+# Instantiate the application tracker variables
 gc = gspread.service_account("service_account.json")
 sh = gc.open("Brick City Bound Management (Responses)")
 sheet = sh.get_worksheet(0)
 titles = sheet.row_values(1)
-cur_day = int(time.strftime('%Y%m%d', time.localtime()))
+last_row = 9
 
-conn = sqlite3.connect('RIT.db')
-c = conn.cursor()
-c.execute("""CREATE TABLE IF NOT EXISTS count (
-id INTEGER,
-messages INTEGER,
-voice_seconds INTEGER
-);""")
-c.execute("""CREATE TABLE IF NOT EXISTS totals (
-day INTEGER,
-voice_hours INTEGER,
-messages INTEGER
-);""")
-conn.commit()
+create_user = lambda id: c.execute("INSERT OR IGNORE INTO user_stats (id, date, message_count, voice_duration) VALUES (?, ?, 0, 0)", (str(id), str(datetime.date.today()),))
+create_server_entry = lambda: c.execute("INSERT OR IGNORE INTO server_stats (date, message_count, voice_duration) VALUES (?, 0, 0)", (str(datetime.date.today()),))
 
-c.execute('SELECT messages FROM totals WHERE day = ?', (cur_day,))
-result = c.fetchone()
-daily_messages = result[0] if result else 0
-c.execute('SELECT voice_seconds FROM totals WHERE day = ?', (cur_day,))
-result = c.fetchone()
-daily_seconds = result[0] if result else 0
-
+def before_shutdown():
+    print("Shutdown requested")
+    conn.commit()
+    conn.close()
+    print("Database closed\n\n")
+    pass
 
 @client.event
 async def on_ready():
-    global bot_channel, role
-    print(f"{client.user} is ready and online!")
-    print(f"Loaded with {daily_messages} messages and {daily_seconds} VC seconds")
-    bot_channel = client.get_channel(1209271549237002310)
+    global role, log_channel
     log_channel = client.get_channel(1191391556033335306)
-    personal_channel = client.get_channel(1210398539855695923)
-    role = client.get_guild(1190760871719338044).get_role(1209635586609250324)
-    guild = client.get_guild(1190760871719338044)
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=str(guild.member_count) + " Members' Every Move"))
-    spreadsheet_loop.start()
+    # Beta bot is not in server
+    if client.get_guild(1190760871719338044):
+        role = client.get_guild(1190760871719338044).get_role(1209635586609250324)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=str(client.get_guild(1190760871719338044).member_count) + " Members' Every Move"))
+    print("Logged in as " + client.user.name)
+    c.execute("SELECT SUM(message_count), SUM(voice_duration) FROM server_stats")
+    result = c.fetchone()
+    print("Total messages retrieved as " + str(result[0]) + "\nTotal voice duration retrieved as " + str(result[1]))
+    if not spreadsheet_loop.is_running():
+        spreadsheet_loop.start()
 
 @client.event
 async def on_message(message):
-    global role, cur_day, daily_messages
-    cur_time = time.localtime()
-        
-    if message.author.bot == False:
-        if int(time.strftime('%Y%m%d', cur_time)) > cur_day:
-            daily_messages = 0
-            cur_day = int(time.strftime('%Y%m%d', cur_day))
-        daily_messages += 1
-        c.execute('SELECT messages FROM totals WHERE day = ?', (cur_day,))
-        result = c.fetchone()
-        if not result:
-            c.execute("INSERT INTO totals (day, voice_hours, messages) VALUES (?, ?, ?)", (cur_day, 0, 1))
-            conn.commit()
-        c.execute("UPDATE totals SET messages = ? WHERE day = ?", (daily_messages, cur_day,))
-        conn.commit()
-
-        
-        c.execute("SELECT id FROM count WHERE id = ?", (message.author.id,))
-        result = c.fetchone()
-        if not result:
-            c.execute("INSERT INTO count (id, messages, voice_seconds) VALUES (?, 1, 0)", (message.author.id,))
-            conn.commit()
-        c.execute("SELECT messages FROM count WHERE id = ?", (message.author.id,))
-        result = c.fetchone()
-        count = result[0]
-        c.execute("UPDATE count SET messages = ? WHERE id = ?", (count+1, message.author.id,))
-        conn.commit()
-        if count+1 == 250:
+    global role, log_channel
+    # We HATE bots
+    if message.author.bot:
+        return
+    create_user(message.author.id)
+    create_server_entry()
+    c.execute("UPDATE user_stats SET message_count = message_count + 1 WHERE id = ? AND date = ?", (str(message.author.id), str(datetime.date.today()),))
+    c.execute("UPDATE server_stats SET message_count = message_count + 1 WHERE date = ?", (str(datetime.date.today()),))
+    conn.commit()
+    
+    c.execute("SELECT SUM(message_count) FROM user_stats WHERE id = ?", (str(message.author.id),))
+    result = c.fetchone()
+    if result and result[0]:
+        if result[0] == 250:
             await message.author.add_roles(role)
-            print("Gave " + message.author.global_name + " Active Member role")
+            print("Gave " + message.author.display_name + " Active Member role")
             embed = discord.Embed(title="Role Assigned", color=0xffffff, description="Active Member role was assigned to " + message.author.global_name + " for reaching 250 messages")
             await log_channel.send(embed=embed)
     await client.process_commands(message)
 
 @client.event
-async def on_member_join(member):
-    try:
-        join_list[2]
-        join_list[0] = join_list[1]
-        join_list[1] = join_list[2]
-        join_list[2] = int(time.time())
-        if join_list[2]-join_list[0] < 60:
-            embed = discord.Embed(title="Suspicious Join Activity", color=0xffffff, description="Three new members have joined in " + str(join_list[2]-join_list[0]) + " seconds.")
-            await log_channel.send(embed=embed)
-            await personal_channel.send("<@803766890023354438>", embed=embed)
-    except IndexError:
-        join_list.append(int(time.time()))
-    if member.guild.id == 1190760871719338044:
-        member_count = member.guild.member_count
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=str(member_count) + " Members' Every Move"))
-        await client.get_channel(1209592495890108456).send(str(member_count))
-
-@client.event
-async def on_member_leave(member):
-    if member.guild.id == 1190760871719338044:
-        member_count = member.guild.member_count
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=str(member_count) + " Members' Every Move"))
-        await client.get_channel(1209592495890108456).send(str(member_count))
-
-@client.event
 async def on_voice_state_update(member, before, after):
-    global voice_dict, daily_seconds, cur_day
-    # User joined voice channel
-    if before.channel == None and after.channel != None:
-        voice_dict[member.id] = int(time.time())
-    # User left voice channel
-    elif before.channel != None and after.channel == None:
-        try:
-            cur_time = time.localtime()
-            if int(time.strftime('%Y%m%d', cur_time)) > cur_day:
-                print("First time processing Voice Activity in a new day")
-                daily_seconds = 0
-                cur_day = int(time.strftime('%Y%m%d', cur_day))
-            daily_seconds += (int(time.time()) - voice_dict[member.id])
-            c.execute('SELECT voice_seconds FROM totals WHERE day = ?', (cur_day,))
-            result = c.fetchone()
-            if not result:
-                c.execute("INSERT INTO totals (day, voice_seconds, messages) VALUES (?, ?, ?)", (cur_day, daily_seconds, 0))
-                conn.commit()
-            c.execute("UPDATE totals SET voice_seconds = ? WHERE day = ?", (daily_seconds, cur_day,))
+    # Catch in case it is the first event that day
+    create_server_entry()
+    # Member joined a channel (Did not move channels)
+    if not before.channel and after.channel:
+        create_user(member.id)
+        c.execute("UPDATE user_stats SET voice_start = ? WHERE id = ? and date = ?", (datetime.datetime.now(), str(member.id), str(datetime.date.today()),))
+        conn.commit()
+    # Member left a join channel (Did not move channels)
+    if before.channel and not after.channel:
+        c.execute("SELECT voice_start FROM user_stats WHERE id = ? AND date = ?", (str(member.id), str(datetime.date.today()),))
+        result = c.fetchone()
+        if result and result[0]:
+            voice_duration = int((datetime.datetime.now() - datetime.datetime.fromisoformat(result[0])).total_seconds())
+            c.execute("UPDATE user_stats SET voice_duration = voice_duration + ? WHERE id = ? AND date = ?", (voice_duration, str(member.id), str(datetime.date.today()),))
+            c.execute("UPDATE server_stats SET voice_duration = voice_duration + ? WHERE date = ?", (voice_duration, str(datetime.date.today()),))
             conn.commit()
 
-            c.execute("SELECT voice_seconds FROM count WHERE id = ?", (member.id,))
-            result = c.fetchone()
-            if not result:
-                c.execute("INSERT INTO count (id, messages, voice_seconds) VALUES (?, 0, 0)", (message.author.id,))
-                conn.commit()
-            c.execute("SELECT voice_seconds FROM count WHERE id = ?", (member.id,))
-            result = c.fetchone()
-            count = result[0] if result[0] else 0
-            c.execute("UPDATE count SET voice_seconds = ? WHERE id = ?", (count+(int(time.time()) - voice_dict[member.id]), member.id,))
-            conn.commit()
-        except Exception as e:
-            # TODO: Save voice_dict on reboot
-            print(e)
-            print("Error in Voice Channel tracking (Did you reboot the bot recently?)")
 
+@client.slash_command()
+@commands.is_owner()
+async def shutdown(ctx):
+    before_shutdown()
+    await ctx.respond("Shutting Down.", ephemeral=True)
+    exit()
+
+@client.slash_command()
+@commands.is_owner()
+async def restart(ctx):
+    before_shutdown()
+    await ctx.respond("Restarting.", ephemeral=True)
+    # Works if you change python3 to be the thing you type in terminal (py, python, python3, python -3.11)
+    os.execv(sys.executable, ['python3'] + sys.argv)
+
+@client.slash_command()
+async def stats(ctx, date: Option(str, "Date in YYYY-MM-DD Format", required = False, default = "Total"), user: discord.User = None): # type: ignore
+    embed = discord.Embed(title="Requested Stats", color=0x00ff00)
+    if user:
+        if date == "Total":
+            c.execute("SELECT SUM(message_count), SUM(voice_duration) FROM user_stats WHERE id = ?", (str(user.id),))
+        else:
+            try:
+                date = datetime.date(int(date[0:4]), int(date[5:7]), int(date[8:10]))
+                c.execute("SELECT message_count, voice_duration FROM user_stats WHERE id = ? AND date = ?", (str(user.id), str(date),))
+            except ValueError as e:
+                print(e)
+                await ctx.respond("Date should look like YYYY-MM-DD, 2024-02-26", ephemeral=True)
+                return
+    else:
+        if date == "Total":
+            c.execute("SELECT SUM(message_count), SUM(voice_duration) FROM server_stats")
+        else:
+            try:
+                date = datetime.date(int(date[0:4]), int(date[5:7]), int(date[8:10]))
+                c.execute("SELECT message_count, voice_duration FROM server_stats WHERE date = ?", (str(date),))
+            except ValueError as e:
+                print(e)
+                await ctx.respond("Date should look like YYYY-MM-DD, 2024-02-26", ephemeral=True)
+                return
+    result = c.fetchone()
+    if result:
+        embed.add_field(name="Date", value=str(date))
+        embed.add_field(name="Message Count", value=result[0])
+        embed.add_field(name="Voice Duration (seconds)", value=result[1])
+    else:
+        embed.description = "No stats found."
+    await ctx.respond(embed=embed, ephemeral=True)
+        
+# TODO: REWRITE THIS PLEASE FOR THE LOVE OF GOD
 @tasks.loop(seconds=10)
 async def spreadsheet_loop():
     global last_row, bot_channel
@@ -183,5 +192,30 @@ async def spreadsheet_loop():
             except:
                 await bot_channel.send("# Too long to send")
 
-client.run(os.getenv('TOKEN'))
-conn.close()
+@client.event
+async def on_member_join(member):
+    global log_channel
+    if member.guild.id == 1190760871719338044:
+        time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        c.execute("INSERT INTO members (member_count, time) VALUES (?, ?)", (member.guild.member_count, time,))
+        conn.commit()
+
+        c.execute("SELECT time FROM members ORDER BY member_count DESC LIMIT 3")
+        result = c.fetchall()
+        recent_joins = [datetime.datetime.strptime(r[0], "%Y-%m-%d %H:%M:%S") for r in result]
+        print(recent_joins)
+        # if len(recent_joins) > 3 and datetime.datetime.now() - latest_entry < datetime.timedelta(seconds=60)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=str(member.guild.member_count) + " Members' Every Move"))
+
+@client.event
+async def on_member_leave(member):
+    # Same notes as 10 lines above
+    if member.guild.id == 1190760871719338044:
+        member_count = member.guild.member_count
+        time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        c.execute("INSERT INTO members (member_count, time) VALUES (?, ?)", (member_count, time,))
+        conn.commit()
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=str(member_count) + " Members' Every Move"))
+
+if __name__ == '__main__':
+    client.run(os.getenv('TOKEN'))
---
+++ temp.py
@@ -0,0 +1,3 @@
+import datetime
+
+print(str(datetime.date(2024, 2, 26)))
